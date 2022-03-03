899ff59a5684febd774470e4b6e34217d2a2643e
ribaorsi <ribaorsi@gmail.com>
2022-03-03 12:15:54 +0100

    vaccination buttonn  added

--- reactinitial/src/components/Clients.jsx
+++ reactinitial/src/components/Clients.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+
+import React, { useState } from "react";
+
 
 const Clients = ({client})=> {
+
+    const [vaccination, setVaccination] = useState(true);
     
         return (
             <div>
                 <p>{client.name}</p>
+                <button onClick={() => setVaccination(true)}>Toggle</button>
                 <p>{client.isVaccinated}</p>
 
             </div>
